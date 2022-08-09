92c89fc73693e220a385e167772894f1f53cc218
xsuzyy <86343090+xsuzyy@users.noreply.github.com>
2022-08-10 00:29:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,25 +16,295 @@
 
 <body>
 
-<?php include('home_navbar.php'); ?>
+<header>
+<a href="#" class="logo" style="font-size: 40px;" ><span><img src="images/logo.png" alt="logo" style="height:80px; width: 80px; margin-left: 5px;margin-right:5px;"></span></a>
+
+	<div id="menu" class="fas fa-bars"></div>
+
+	<nav class="navbar">
+        <a class="active" href="#home">home</a>
+        <a href="#course">Courses</a>
+        <a href="#sch">Scholarship</a>
+        <a href="#abt">About Us</a>
+    </nav>
+
+</header>
 <section id="home" style="margin-bottom: -110px;">
-<?php include('home_main.php'); ?>
+<div class="video-header wrap">
+	<div class="fullscreen-video-wrap">
+		<img src="bg.gif" alt="bg">
+	</div>
+
+	<div class="header-overlay">
+		<div class="header-content">
+			<h1>International State College of the Philippines<br>Atlantis Campus</h1>
+			
+		</div>
+	</div>
+
+	<div class="header-overlay-1" >
+		<a href="" class="btn-1">ENROLL NOW</a> <br>
+	</div>
+	
+</div>
 </section>
 <section id="course" class="course">
-<?php include('home_course.php'); ?>
+<div class="row">
+
+    <div class="image">
+            <img src="images/kuya_kim.png" alt="">
+    </div>
+
+    <div class="content">
+        <h1>What<br> we offer</h1>
+
+        <div class="slide-container swiper">
+        
+            <div class="slide-content">
+
+                    <div class="card-wrapper swiper-wrapper">
+                    <?php
+
+                    ?>
+                        <div class="card swiper-slide">
+                            <div class="image-content">
+                                <span class="overlay"></span>
+
+                                <div class="card-image">
+                                    <img src="images/logo.png" class="card-img">
+                                </div>
+                            </div>
+
+                            <div class="card-content">
+                                <h2 class="name">aa</h2>
+                            </div>
+                        </div>
+
+                        <div class="card swiper-slide">
+                            <div class="image-content">
+                                <span class="overlay"></span>
+
+                                <div class="card-image">
+                                    <img src="images/test.jpg" class="card-img">
+                                </div>
+                            </div>
+
+                            <div class="card-content">
+                                <h2 class="name">aa</h2>
+
+                            </div>
+                        </div>
+
+
+                        
+ 
+                    </div>
+
+            </div>
+
+                <div class="swiper-button-next swiper-navBtn"></div>
+                <div class="swiper-button-prev swiper-navBtn"></div>
+                <div class="swiper-pagination"></div>
+
+        </div>
+    
+    </div>
+          
+  
+
+</div>
 </section>
 <section id="sch" class="sch" style="margin-top:0px;">
-<?php include('home_scholarship.php'); ?>
+<div class="column">
+
+        <div class="box-container">
+
+            <h3 class="title">Global Scholarship</h3>
+
+            <div class="box">
+       
+                <div class="info">
+                    <h3>ISCP Scholarship | 100%</h3>
+                    <p>Free tuition capped at 50 pesos</p>
+                    <a href="" class="btn"> Apply for ISCP Scholar</a>
+                </div>
+              
+            </div>
+
+            <div class="box">
+       
+                <div class="info">
+                    <h3>Sun and Moon | 100%</h3>
+                    <p>Free sun and moon capped at 100 pesos</p>
+                    <a href="" class="btn"> Apply for Sun and Moon Scholar</a>
+                </div>
+             
+            </div>
+
+            <div class="box">
+
+                <div class="info">
+                    <h3>Baby Baby Baby | 100%</h3>
+                    <p>Free ISCP poster capped at 120 pesos.</p>
+                    <a href="" class="btn"> Apply for Baby Baby Baby  Scholar</a>
+                </div>
+               
+            </div>
+
+        </div>
+
+        <div class="box-container">
+             <h3 class="title">Atlantis Exclusive Scholarship</h3>
+
+                <div class="box">
+
+                    <div class="info">
+                        <h3>Atlanteans Scholarship | 100%</h3>
+                        <p>Free tuition capped at 50 pesos</p>
+                        <a href="" class="btn"> Apply for ISCP Scholar</a>
+                    </div>
+                
+                </div>
+
+                <div class="box">
+
+                    <div class="info">
+                        <h3>Poseidon Buff | 100%</h3>
+                        <p>Free Unlimited Lung Capacity</p>
+                        <a href="" class="btn"> Apply for Poseidon Scholar</a>
+                    </div>
+                
+                </div>
+
+          
+        </div>
+
+    </div>
 </section>
 <section id="abt" class="abt" style="margin-top:0px;">
-<?php include('home_about.php'); ?>
+<div class="row">
+
+   
+
+    <div class="content">
+        <h1>About Us</h1>
+        <p>On 1598, International State College of the Philippines, Atlantis Campus, <br>
+        the first college university in the underwater, was founded.</p>
+    </div>
+    <div class="content">
+        <h2>Mission & Vision</h2>
+        <p>Araw at gabi hanap-hanap ka sa magdamag <br>
+            Sa dami ng pagpipilian ikaw lang ang naibigan ko na mahalin<br>
+            Aalayan ka ng bulaklak mapangiti ka lang ay ayos na<br>
+            Ikaw sa'kin ang tinadhana, ang gusto kong makasama Hanggang sa tumanda</p>
+
+     <h2>ISCP Hymn</h2>
+          <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+				Hymn Video
+			</button>
+
+			<!-- Modal -->
+			<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+				<div class="modal-dialog modal-lg">
+					<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+						<video src="../images/video/poso.mp4" style="width: 100%; margin: auto;" controls></video>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn" style="background-color:red;" data-bs-dismiss="modal">Close</button>
+					</div>
+					</div>
+				</div>
+			</div>
+    </div>
+			
+
+    <div class="image">
+            <img src="images/pos.png" alt="">
+    </div>
+          
+  
+
+</div>
 </section>
 
 <a href="#home" class="scroll-top">
     <img src="images/up.png" alt="up_image">
 </a>
 
-<?php include('home_footer.php');?>
+<div class="footer">
+
+    <h1 class="credit">The satirical website of ISCPhilippines.</h1>
+
+</div>
 </body>
-<?php include('scripts.php');?>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/swipe.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('load scroll',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if($(window).scrollTop() > 0){
+      $('header').addClass('sticky');
+    }else{
+      $('header').removeClass('sticky');
+    }
+
+    if($(window).scrollTop() > 0){
+      $('.scroll-top').show();
+    }else{
+      $('.scroll-top').hide();
+    }
+
+    // scroll spy 
+
+    $('section').each(function(){
+
+      let top = $(window).scrollTop();
+      let offset = $(this).offset().top - 200;
+      let id = $(this).attr('id');
+      let height = $(this).height();
+
+      if(top > offset && top < offset + height){
+        $('.navbar a').removeClass('active');
+        $('.navbar').find(`[href="#${id}"]`).addClass('active');
+      }
+
+    });
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
+</script>
+
 </html>
